f570de1b548297c64c16dd61e432f73327ce03cc
CianTivnan <cian.tivnan@yahoo.ie>
2021-01-28 09:43:51 +0000

    Task 8 complete

--- Week1/Band.cs
+++ Week1/Band.cs
@@ -6,14 +6,64 @@ using System.Threading.Tasks;
 
 namespace Week1
 {
-    class Band
+    abstract class Band : IComparable<Band>
     {
+        //attributes
         public string BandName { get; set; }
 
         public int YearFormed { get; set; }
 
         public string Members { get; set; }
 
+        //ctors
+        public Band() { }
 
+        public Band(string a, int b, string c)
+        {
+            BandName = a;
+            YearFormed = b;
+            Members = c;
+        }
+
+        
+
+        public int CompareTo(Band band)
+        {
+            return BandName.CompareTo(band.BandName);
+        }
     }
+
+    class RockBand : Band
+    {
+        //ctor
+        public RockBand(string a, int b, string c) : base(a, b, c) { }
+
+        public override string ToString()
+        {
+            return BandName + " - Rock";
+        }
+    }
+
+    class PopBand : Band
+    {
+        //ctor
+        public PopBand(string a, int b, string c) : base(a, b, c) { }
+
+        public override string ToString()
+        {
+            return BandName + " - Pop";
+        }
+    }
+
+    class IndieBand : Band
+    {
+        //ctor
+        public IndieBand(string a, int b, string c) : base(a, b, c) { }
+
+        public override string ToString()
+        {
+            return BandName + " - Indie";
+        }
+    }
+
 }
--- Week1/MainWindow.xaml.cs
+++ Week1/MainWindow.xaml.cs
@@ -20,17 +20,44 @@ namespace Week1
     /// </summary>
     public partial class MainWindow : Window
     {
+        static List<Band> DisplayList = new List<Band>();
+        static List<Band> BandList = new List<Band>();
 
         public MainWindow()
         {
             InitializeComponent();
 
             LoadBands();
+
+            DisplayBands();
         }
 
         private void LoadBands()
         {
+            RockBand b1 = new RockBand("Woods of Ypres", 2002, "David Gold, Joel Violette, Rae Amitay, Brendan Hayter");
+            BandList.Add(b1);
+            DisplayList.Add(b1);
+            RockBand b2 = new RockBand("Behemoth", 1991, "Adam Darski, Zbigniew Prominski, Tomasz Wroblewski");
+            BandList.Add(b2);
+            DisplayList.Add(b2);
+            PopBand b3 = new PopBand("Scooter", 1993, "Hans Peter Geerdes, Michael Simon, Sebastian Schilde");
+            BandList.Add(b3);
+            DisplayList.Add(b3);
+            PopBand b4 = new PopBand("Cascada", 2004, "Natalie Horlier, Manuel Reuter, Yann Peifer");
+            BandList.Add(b4);
+            DisplayList.Add(b4);
+            IndieBand b5 = new IndieBand("Dread Sovereign", 2013, "Alan Nemtheanga Averill, Eoin Bones, Johnny King");
+            BandList.Add(b5);
+            DisplayList.Add(b5);
+            IndieBand b6 = new IndieBand("Cruachan", 1992, "Keith Fay, Joe Farrell, Audrey Trainor, Dave Quinn, Tom Woodlock");
+            BandList.Add(b6);
+            DisplayList.Add(b6);
+        }
 
+        private void DisplayBands()
+        {
+            DisplayList.Sort();
+            lbx_Bands.ItemsSource = DisplayList;
         }
     }
 }
